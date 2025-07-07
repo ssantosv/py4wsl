2eca49c94016f49a3b8c86d862d11854dc11b953
ssantosv <s.delossantos@gmail.com>
2025-07-07 10:56:12 +0200

    Ok

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='PyWSL',
-    version='0.1.0',
+    name='Py4WSL',
+    version='0.0.1',
     packages=find_packages(),
-    url='https://github.com/ssantosv/pywsl',  # Reemplaza con tu URL
+    url='https://github.com/ssantosv/py4wsl',  # Reemplaza con tu URL
     license='MIT',
     author='Sergio de los Santos',  # Reemplaza con tu nombre
     author_email='s.delossantos@gmail.com',  # Reemplaza con tu email
